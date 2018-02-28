84d47e97f9abf688b3dde3dea295c9fc7939a59d
yqwoe <yqwoe@live.cn>
2018-02-28 20:12:41 +0800

    增加测试

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  #
+  def new_product(image_url)
+    Product.new({
+      :title => "Apple,MacBook Pro 15",
+      :desc => "笔记本电脑",
+      :image_url => image_url,
+      :price => "18288"
+    })
+  end
+  test ' product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:desc].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'My Book title',desc: 'yyy',image_url: 'abc.jpg')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test 'image url' do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more}
+    ok.each do |name|
+      assert new_product(name).valid?,"#{name} shouldn't be invalid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?,"#{name} shouldn't be valid"
+    end
+  end
+  test 'product is not valid without a unique title' do
+    product = Product.new(:title => products(:ruby).title,:desc => 'yyyy',price: 1,image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal ["has already been taken"],product.errors[:title]
+  end
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(:title => products(:ruby).title,:desc => 'yyyy',price: 1,image_url: 'fred.gif')
+    assert product.invalid?
+    puts I18n.translate('errors.messages.taken')
+    assert_equal [I18n.translate('errors.messages.taken')],product.errors[:title]
+  end
 end
